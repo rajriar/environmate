83aa79d0fd2d4a398cb0998d1d713e51edd4fe81
Rajdeep Riar <rajriar93@gmail.com>
2019-06-19 00:11:47 -0700

    Added about team page

--- application/EnvironmentalIssues/routes/index.js
+++ application/EnvironmentalIssues/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CSC 648 Team 1 Home Page' });
+    res.render('index', { title: 'CSC 648 Team 1 Home Page' });
+});
+
+//about page
+router.get("/about", function(req, res, next) {
+    res.render("about.ejs");
 });
 
 module.exports = router;
