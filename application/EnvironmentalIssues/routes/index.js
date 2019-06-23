8f865bb8932f4acf22278a840d5d234e52a19041
Michael Gilbert <gilbertmichael888@gmail.com>
2019-06-23 14:26:58 -0700

    set up about-page routes (#11)

--- application/EnvironmentalIssues/routes/index.js
+++ application/EnvironmentalIssues/routes/index.js
@@ -6,12 +6,4 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'CSC 648 Team 1 Home Page' });
 });
 
-<<<<<<< HEAD
-=======
-//about page
-router.get('/about', function(req, res, next) {
-    res.render('about/about', { title: 'Team 1',});
-});
-
->>>>>>> fe5a2422cce14063e85e131bd397cee1d24461e9
 module.exports = router;
