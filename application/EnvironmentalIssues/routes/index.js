0e49500e2cca97c5caa30c74f45845e06c5286a2
rajriar <47168162+rajriar@users.noreply.github.com>
2019-06-23 22:12:40 -0700

    Raj (#10)
    * Updated about team, added template for individual team members page
    
    * Updated my about me personal page
    
    * Changed my ejs file name to raj.ejs
    
    * removed dummy text
    
    * moved raj.ejs to the about folder

--- application/EnvironmentalIssues/routes/index.js
+++ application/EnvironmentalIssues/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'CSC 648 Team 1 Home Page' });
 });
-
+//about page
+router.get('/about', function(req, res, next) {
+    res.render('about/about', { title: 'Team 1', });
+});
 module.exports = router;
