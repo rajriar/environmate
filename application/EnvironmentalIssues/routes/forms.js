6ae1740ae54db3a9baa8869b1caea08129f04dd0
Jonathan Julian <jjulian2@mail.sfsu.edu>
2019-07-06 07:12:28 -0700

    642 project update -- captcha working -- gmaps not

--- application/EnvironmentalIssues/routes/forms.js
+++ application/EnvironmentalIssues/routes/forms.js
@@ -1,28 +1,25 @@
 const express = require('express');
 var router = express.Router();
 const app = express();
+var bodyParser = require('body-parser');
 
-var Recaptcha = require('express-recaptcha').RecaptchaV3;
-//import Recaptcha from 'express-recaptcha'
-var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY');
-//or with options
-var options = {'hl':'de'};
-var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY', options);
 
 app.use(express.urlencoded({extended: true}));
-const {check, validationResult} = require('express-validator/check');
+
 
 //642 project
 router.get('/jonathanjulian', function (req, res, next){
-    res.render('about/forms/jonathanjulian',{title: '642 personal form', captcha:res.recaptcha});
+    res.render('about/forms/jonathanjulian',{title: '642 personal form',});
 });
 
 router.get('/confirmation', function (req, res, next){
-    res.send(req.check('firstName').isAlpha());
+    res.send('confirmation fail');
 });
 
 router.post('/confirmation', function (req, res, next){
+    
     res.render('about/forms/confirmation', {data: req.body});
+        
 });
 
 module.exports = router;
