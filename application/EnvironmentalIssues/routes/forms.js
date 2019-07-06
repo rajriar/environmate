f2bf140c1ceee14fef3c0c13337a04fb85112a5d
Jonathan Julian <jjulian2@mail.sfsu.edu>
2019-07-05 17:47:50 -0700

    updated personal project last name req

--- application/EnvironmentalIssues/routes/forms.js
+++ application/EnvironmentalIssues/routes/forms.js
@@ -2,12 +2,19 @@ const express = require('express');
 var router = express.Router();
 const app = express();
 
+var Recaptcha = require('express-recaptcha').RecaptchaV3;
+//import Recaptcha from 'express-recaptcha'
+var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY');
+//or with options
+var options = {'hl':'de'};
+var recaptcha = new Recaptcha('SITE_KEY', 'SECRET_KEY', options);
+
 app.use(express.urlencoded({extended: true}));
 const {check, validationResult} = require('express-validator/check');
 
 //642 project
 router.get('/jonathanjulian', function (req, res, next){
-    res.render('about/forms/jonathanjulian',{title: '642 personal form',});
+    res.render('about/forms/jonathanjulian',{title: '642 personal form', captcha:res.recaptcha});
 });
 
 router.get('/confirmation', function (req, res, next){
