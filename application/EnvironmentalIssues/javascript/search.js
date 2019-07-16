7ca35ba4f5a074a91e25e1e25d96612b908e5c60
Rajdeep Riar <rajriar93@gmail.com>
2019-07-15 23:33:52 -0700

    updated navbar

--- application/EnvironmentalIssues/javascript/search.js
+++ application/EnvironmentalIssues/javascript/search.js
@@ -1,22 +1,23 @@
-const db = require('./dbConnection');
+//const db = require('./dbConnection');
 
-var find = function(request, callback){
+var find = function(request, callback) {
 
-    var sql = "SELECT  u.*,r.role_name FROM USERS u INNER JOIN ROLES r on u.role = r.role_id WHERE u."+request.query.search_field +" like '%" + request.query.search_text + "%'";
-    if(request.query.search_field === 'ROLE'){
-        sql = "SELECT u.*,r.role_name FROM USERS u INNER JOIN ROLES r on u.role = r.role_id where r.role_name like '%"
-                +request.query.search_text + "%'";
+    var sql = "SELECT  u.*,r.role_name FROM USERS u INNER JOIN ROLES r on u.role = r.role_id WHERE u." + request.query.search_field + " like '%" + request.query.search_text + "%'";
+    if (request.query.search_field === 'ROLE') {
+        sql = "SELECT u.*,r.role_name FROM USERS u INNER JOIN ROLES r on u.role = r.role_id where r.role_name like '%" +
+            request.query.search_text + "%'";
     }
-    db.query(sql, function (err, result) {
+    db.query(sql, function(err, result) {
         if (err) callback(err, null);
-        else{ callback(null, result);
-            
-        } 
+        else {
+            callback(null, result);
+
+        }
     });
 
 }
 
-var close = function(request){
+var close = function(request) {
     db.end();
 }
 
