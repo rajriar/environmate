603168c7094c0aa4c9a58184e9af212b7e6db305
Jonathan Julian <45022382+jjulian91@users.noreply.github.com>
2019-07-16 13:22:53 -0700

    updated local db route

--- application/EnvironmentalIssues/javascript/dbConnection.js
+++ application/EnvironmentalIssues/javascript/dbConnection.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql')
 var connection = mysql.createConnection({
-  host: '54.67.108.149', //change to Localhost
+  host: '127.0.0.1', //change to Localhost
   user: 'root', //change to root
   password: 'csc6481',
   database: 'EnvironMate'
