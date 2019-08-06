18d97cc4a35f78bdc0eadc6d62d44807fe2dc665
Jonathan Julian <jjulian2@mail.sfsu.edu>
2019-08-06 12:24:32 -0700

    updated search.js full text search works

--- application/EnvironmentalIssues/javascript/search.js
+++ application/EnvironmentalIssues/javascript/search.js
@@ -1,12 +1,70 @@
-const models = require('../models');
-const sequelize = require ('sequelize');
-const op = sequelize.Op;
+/*
+Author : Jonathan Julian
+Function : provides search and returns all if no text is searched.
+Date: 8/6/2019
+
+*/
+
+
+const models = require('../models'); //map to model folder
+const sequelize = require ('sequelize'); //import statement for sequelize
+const op = sequelize.Op; //shortcut to operations in sequelize
 
 var find = function(request, callback) {
     var results=[];
-    if(request.query.search_text === ""){
+    if(request.query.search_text === ""){ //checks for no text returns all entries
         models.incidents.findAll({
-            include: [
+            include: [ //includes associations defined in models
+                {
+                    association: 'Location',
+                    include:[ //2nd level association in location model
+                        { 
+                            association: 'Zipcode',
+                            required: true
+                        }
+
+                    ],
+                    required: true //required true == inner join 
+                },
+                {
+                    association: 'Status',
+                    required: true
+                },
+                {
+                    association: 'Type',
+                    required: true
+                },
+                {
+                    model: models.image,
+                    required: false //return false == left outter join
+                }
+            ]
+        }).then(incidents =>{
+            callback(null, incidents) //returns incidents to front
+        });
+    }
+    else{ //text has been searched.
+        models.incidents.findAll({
+            where:{ //search criteria
+                [op.or]:{ //or condition
+                    description:{ //searches main table
+                        [op.like] : "%"+request.query.search_text+"%" //%like search
+                    },
+                    '$Location.locationName$': { //associates table look up as maintable.AssociatedtableName.tableColumn
+                        [op.like] : "%"+request.query.search_text+"%"  
+                    },
+                    '$Location.Zipcode.zipCode$' : { //2 layers of associated tables
+                        [op.like] : "%"+request.query.search_text+"%"
+                    },
+                    '$Status.statusName$':{
+                        [op.like] : "%"+request.query.search_text+"%"
+                    },
+                    '$Type.typeName$':{
+                        [op.like] : "%"+request.query.search_text+"%"
+                    }
+                }
+            },
+            include: [ //includes the tables to resolve the host information. 
                 {
                     association: 'Location',
                     include:[
@@ -14,7 +72,6 @@ var find = function(request, callback) {
                             association: 'Zipcode',
                             required: true
                         }
-
                     ],
                     required: true
                 },
@@ -31,28 +88,9 @@ var find = function(request, callback) {
                     required: false
                 }
             ]
-        }).then(incidents =>{
-            console.log(incidents)
-            callback(null, incidents)
-        });
-    }
-    else{
-        models.incidents.findAll({
-            where: {
-                [op.or]:{
-                    description :{
-                        [op.like] : '%'+request.query.search_text+"%" 
-                    }
-                }
-            },
-            include:[
-                {all: true}
-            ]
         }).then(incidents => {
-            results = incidents;
+                callback(null,incidents); //returns incidents
         });
-        results = JSON.parse(JSON.stringify(results));
-        callback(null, results);
     }
 }
 
