81cba1dce3b18be973cbe906858f0218b016730a
Rajdeep Riar <rajriar93@gmail.com>
2019-08-06 18:10:44 -0700

    Changed timing issue

--- application/EnvironmentalIssues/routes/login.js
+++ application/EnvironmentalIssues/routes/login.js
@@ -8,41 +8,41 @@ app.use(cookieParser);
 
 const models = require('../models');
 
-router.use(function (req, res, next) {
-  // let cookieName = req.cookies.cookieName;
-  // if(!cookieName) {
-  //   let newCookie = Math.random().toString();
-  //   res.cookie('cookieName', newCookie);
-  // }
-  // console.log("cooke: " + cookieName);
-  next();
+router.use(function(req, res, next) {
+    // let cookieName = req.cookies.cookieName;
+    // if(!cookieName) {
+    //   let newCookie = Math.random().toString();
+    //   res.cookie('cookieName', newCookie);
+    // }
+    // console.log("cooke: " + cookieName);
+    next();
 })
 
 router.post('/', (req, res, next) => {
     console.log('req.body');
     console.log(req.body);
     models.users.findOne({
-      where: {
-        userEmail: req.body.userEmail
-      }
+        where: {
+            userEmail: req.body.userEmail
+        }
     }).then(user => {
-      console.log("compare password: " + user.comparePassword(req.body.password));
-      console.log(!user);
-      if (!user.comparePassword(req.body.password) || user == null) {
-        res.status(401).json({ token: null, errorMessage: 'failed!' })
-      } else {
-        res.cookie("user",{
-          firstName   : user.firstName,
-          lastName    : user.lastName,
-          email       : user.userEmail,
-          id          : user.userId,
-          role        : user.idRole
-        });
-        
-        console.log("logged in as: ", user.dataValues);
-      }
-      
-      res.status(204).send();
+        console.log("compare password: " + user.comparePassword(req.body.password));
+        console.log(!user);
+        if (!user.comparePassword(req.body.password) || user == null) {
+            res.status(401).json({ token: null, errorMessage: 'failed!' })
+        } else {
+            res.cookie("user", {
+                firstName: user.firstName,
+                lastName: user.lastName,
+                email: user.userEmail,
+                id: user.userId,
+                role: user.idRole
+            });
+
+            console.log("logged in as: ", user.dataValues);
+        }
+
+        res.status(204).send();
     });
 });
 
